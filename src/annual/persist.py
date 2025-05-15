a43dea80835b13d65d5dc2fb1d25f01f9e1e9458
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-15 23:05:04 +0100

    Add src/annual/persist.py

---
+++ src/annual/persist.py
@@ -0,0 +1,90 @@
+"""Module persist.py"""
+import json
+import os
+
+import pandas as pd
+
+import config
+import src.functions.directories
+import src.functions.objects
+
+
+class Persist:
+    """
+    Persist
+    """
+
+    def __init__(self, reference: pd.DataFrame, frequency: float):
+        """
+
+        :param reference: Each instance encodes a few gauge attributes/characteristics
+        :param frequency: The granularity of the data, in hours.
+        """
+
+        self.__reference = reference
+        self.__interval = frequency * 60 * 60 * 1000
+
+        # The storage area
+        self.__configurations = config.Config()
+        self.__endpoint = os.path.join(self.__configurations.points_, 'annual')
+
+        # Ensure the storage area exists
+        src.functions.directories.Directories().create(self.__endpoint)
+
+        # For creating JSON files
+        self.__objects = src.functions.objects.Objects()
+
+    @staticmethod
+    def __get_nodes(data: pd.DataFrame) -> dict:
+        """
+
+        :param data: A frame wherein each field's data is the data of a distinct gauge, and the
+                     gauges belong to the same catchment.
+        :return:
+        """
+
+        frame = data.copy().drop(columns='milliseconds')
+
+        dictionaries = [json.loads(data[column].to_json(orient='split')) for column in frame.columns]
+        periods = [dictionaries[i]['name'] for i in range(len(dictionaries))]
+        sections = [dictionaries[i]['data'] for i in range(len(dictionaries))]
+
+        nodes = {'periods': periods, 'data': sections}
+
+        return nodes
+
+    def __get_attributes(self, ts_id: int) -> pd.DataFrame:
+        """
+
+        :param ts_id:
+        :return:
+        """
+
+        frame: pd.DataFrame = self.__reference.loc[self.__reference['ts_id'] == ts_id, :]
+        attributes = frame.copy().drop_duplicates(ignore_index=True)
+
+        return attributes
+
+    def exc(self, data: pd.DataFrame, ts_id: int) -> str:
+        """
+
+        :param data:
+        :param ts_id:
+        :return:
+        """
+
+        # Nodes vis-à-vis the data fields only
+        nodes = self.__get_nodes(data=data.copy())
+
+        # Attributes
+        attributes = self.__get_attributes(ts_id=ts_id)
+
+        # Hence
+        nodes['starting'] = self.__configurations.leap
+        nodes['interval'] = self.__interval
+        nodes['attributes'] = attributes.to_dict(orient='split')
+
+        message = self.__objects.write(
+            nodes=nodes, path=os.path.join(self.__endpoint, f'{str(ts_id)}.json'))
+
+        return message
