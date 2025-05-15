5138883cbe7a57ae68c4ba63954758a8c93aba26
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-15 22:23:30 +0100

    Update src/annual/data.py

--- src/annual/data.py
+++ src/annual/data.py
@@ -1,9 +1,10 @@
 """Module data.py"""
-import logging
+import datetime
 
 import numpy as np
 import pandas as pd
 
+import config
 import src.elements.text_attributes as txa
 import src.functions.streams
 
@@ -20,41 +21,77 @@ class Data:
 
         self.__fields = {'timestamp': np.int64, 'measure': np.float64}
 
+        # Configurations
+        self.__configurations = config.Config()
+
+        # An instance for reading CSV files
         self.__streams = src.functions.streams.Streams()
 
-    def __measures(self, uri: str, name: str) -> pd.DataFrame:
+    def __get_data(self, uri: str) -> pd.DataFrame:
         """
 
         :param uri: A uniform resource identifier
-        :param name: A first day of the year string, e.g., 2023-01-01.
+
         :return:
         """
 
         text = txa.TextAttributes(uri=uri, header=0, usecols=list(self.__fields.keys()), dtype=self.__fields)
         data = self.__streams.read(text=text)
-
-        data.rename(columns={'measure': name}, inplace=True)
-
         data.sort_values(by='timestamp', ascending=True, inplace=True)
         data.drop_duplicates(subset=['timestamp'], keep='first', inplace=True)
+        data['date'] = pd.to_datetime(data['timestamp'], unit='ms')
 
         return data
 
-    def exc(self, listing: pd.DataFrame):
+    def __get_milliseconds(self, blob: pd.DataFrame, date: pd.Timestamp):
         """
 
-        :param listing: Includes a field of uniform resource identifiers for data acquisition, additionally
-                        each instance includes a time series identification code
+        :param blob:
+        :param date:
         :return:
         """
 
-        dates = listing['date'].unique()
+        datestr = date.strftime('%Y-%m-%d')
 
-        instances = []
-        for date in dates:
-            uri = listing.loc[listing['date'] == date, 'uri'].values[0]
-            frame = self.__measures(uri=uri, name=str(date))
-            instances.append(frame)
-        logging.info(instances)
+        if (date.year % 4 ) == 0:
+            blob['milliseconds'] = (blob['date'] - pd.Timestamp(datestr)) // pd.Timedelta("0.001s")
+        else:
+            boundary = datetime.datetime.strptime(f'{date.year}-03-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+            states = (blob['date'] >= boundary).astype(int).values
 
-        return len(instances)
+            blob['r_milliseconds'] = (blob['date'] - pd.Timestamp(datestr)) // pd.Timedelta("0.001s")
+            blob = blob.assign(milliseconds = blob['r_milliseconds'].values + (self.__configurations.shift * states) )
+
+        return blob
+
+    @staticmethod
+    def __limits(blob: pd.DataFrame, date: pd.Timestamp):
+        """
+
+        :param blob:
+        :param date:
+        :return:
+        """
+
+        lower = datetime.datetime.strptime(f'{date.year}-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+        upper = datetime.datetime.strptime(f'{date.year}-12-31 23:59:59', '%Y-%m-%d %H:%M:%S')
+
+        return blob.loc[(blob['date'] >= lower) & (blob['date'] <= upper), :]
+
+    def exc(self, uri: str, date: pd.Timestamp):
+        """
+
+        :param uri: A uniform resource identifier
+        :param date: A first day of the year string, e.g., 2023-01-01.
+        :return:
+        """
+
+        data = self.__get_data(uri=uri)
+        data = self.__get_milliseconds(blob=data.copy(), date=date)
+        data = self.__limits(blob=data.copy(), date=date)
+
+
+        instances = data.copy()[['milliseconds', 'measure']]
+        instances.rename(columns={'measure': str(date)}, inplace=True)
+
+        return instances
