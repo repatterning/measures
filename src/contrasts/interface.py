397db2230ce4523ade8e7c6b1d7b53cf29acf603
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-15 02:35:36 +0100

    Update src/contrasts/interface.py: Under Development

--- src/contrasts/interface.py
+++ src/contrasts/interface.py
@@ -1,14 +1,19 @@
+
 import logging
 
+import dask
 import numpy as np
 import pandas as pd
-import dask
 
-import src.elements.partitions as pr
 import src.contrasts.data
 import src.contrasts.persist
+import src.elements.partitions as pr
+
 
 class Interface:
+    """
+    Interface
+    """
 
     def __init__(self, listings: pd.DataFrame, reference: pd.DataFrame, arguments: dict):
         """
@@ -36,19 +41,21 @@ class Interface:
     def exc(self, partitions: list[pr.Partitions]):
         """
 
-        :param partitions:
+        :param partitions: Refer to src/elements/partitions.py for more about Partitions
         :return:
         """
 
         catchment_id_ = np.array([partition.catchment_id for partition in partitions])
         catchment_id_ = np.unique(catchment_id_)
 
+        # Delayed Tasks
         __get_data = dask.delayed(src.contrasts.data.Data().exc)
         __persist = dask.delayed(src.contrasts.persist.Persist(
             reference=self.__reference, frequency=self.__arguments.get('frequency')).exc)
 
+        # Compute
         computations = []
-        for catchment_id in catchment_id_[:2]:
+        for catchment_id in catchment_id_[:8]:
             listing = self.__get_codes(catchment_id=catchment_id)
             data = __get_data(listing=listing)
             message = __persist(data=data, catchment_id=catchment_id)
