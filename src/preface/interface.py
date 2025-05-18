3b59884bdf5048854ca3a572757f83e256ef760a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-05-18 20:06:49 +0100

    Update src/preface/interface.py

--- src/preface/interface.py
+++ src/preface/interface.py
@@ -38,9 +38,10 @@ class Interface:
 
         return arguments
 
-    def exc(self) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
+    def exc(self, codes: list[int]) -> typing.Tuple[boto3.session.Session, s3p.S3Parameters, sr.Service, dict]:
         """
 
+        :param codes: A set of gauge time series codes
         :return:
         """
 
@@ -49,6 +50,11 @@ class Interface:
         service: sr.Service = src.functions.service.Service(
             connector=connector, region_name=s3_parameters.region_name).exc()
         arguments: dict = self.__get_arguments(connector=connector)
+
+        if codes is None:
+            arguments['excerpt'] = []
+        else:
+            arguments['excerpt'] = codes
 
         src.preface.setup.Setup(
             service=service, s3_parameters=s3_parameters).exc()
